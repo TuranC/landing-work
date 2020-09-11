a8a3d18bc33d03ab1902168beb261c02d0bccc5d
TuranC <turancelebi15@icloud.com>
2020-09-11 16:31:05 +0300

    remove min.png

--- src/app/minImages.js
+++
@@ -1,9 +0,0 @@
-import '../public/min-images/clock.min.png';
-import '../public/min-images/face.min.png';
-import '../public/min-images/girl.min.png';
-import '../public/min-images/laptop-money.min.png';
-import '../public/min-images/milk.min.png';
-import '../public/min-images/people.min.png';
-import '../public/min-images/tasks.min.png';
-import '../public/min-images/zero.min.png';
-import '../public/min-images/logo.min.png';
